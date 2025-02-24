e1aefcc0344bd615b276b6ea2a196ec684cbe33c
Manikanta <smanikanta1239@gmail.com>
2025-02-24 20:23:24 +0530

    to create user defined modules and using them

---
+++ pack.js
@@ -0,0 +1,10 @@
+const mat = require('./math');
+
+console.log(mat.add(2,3));
+console.log(mat.sub(2,3));
+
+
+const { add, sub } = require('./math');
+
+console.log(add(4,3));
+console.log(sub(4,3));
